360406cde98d2d4014e2509478e80acae609a4c1
yuunees.s <younes.elazzouzi@student.kdg.be>
2025-05-30 22:33:09 +0200

    ChatGPT Proefexamen + Examen Template added

---
+++ CS2 - ISB Powershell/Scripts/Get-Mac - template.ps1
@@ -0,0 +1,25 @@
+﻿# Name: Get-Mac.ps1
+# Author:
+# Date:
+
+$filename = "....\MacTable.csv"
+
+function Delete-LinesCsv($property, $value, $filename) {
+    Import-Csv $filename | ? { $_.... } | Export-Csv ($filename + ".new") -force
+    Move-Item ....
+    }
+
+function Write-Mac() {
+    ....
+    }
+
+function Get-Mac($computername) {
+    ....
+    }
+
+function Clean-Mac([int]$maxtimespan) {
+    ....
+    }
+
+# Hoofdprogramma
+....
